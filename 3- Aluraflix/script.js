3106e36ec5e1630254c505090cf5d8620f3d1f4e
Lorenzo Librelon <lorenzo_librelon@yahoo.com>
2023-02-02 09:01:52 -0300

    Atualizações das Aulas
    Att. Aula 3

---
+++ 3- Aluraflix/script.js
@@ -0,0 +1,16 @@
+
+    var listaSeries = [];
+    listaSeries[0] = "https://m.media-amazon.com/images/I/81MTILmqkXL._AC_SL1500_.jpg";
+    listaSeries[1] = "https://m.media-amazon.com/images/M/MV5BM2QyNDIzOGMtNThhNS00NmUwLWI0ZjUtZjdkN2I1OTRjZWQ3XkEyXkFqcGdeQXVyNzQ1ODk3MTQ@._V1_.jpg";
+    listaSeries[2] = "https://images.amcnetworks.com/amctv.pt/wp-content/uploads/2021/09/BREAKING-BAD-T5-MOBILE.jpg";
+    listaSeries[3] = "https://s2.glbimg.com/DzR7zPG_OQCRWCu921VBzdGg-0k=/362x536/https://s2.glbimg.com/p3pC3FBT8LOPZIpQade0SRCt4Kg=/i.s3.glbimg.com/v1/AUTH_c3c606ff68e7478091d1ca496f9c5625/internal_photos/bs/2019/E/U/a6J1gGRsWBEtuwQjt1uA/2019-296-series-dexter-poster.jpg";
+    listaSeries[4] = "https://br.web.img3.acsta.net/c_310_420/pictures/19/08/07/17/01/2286697.jpg";
+    listaSeries[5] = "https://br.web.img2.acsta.net/c_310_420/pictures/20/12/02/16/22/0450183.jpg";
+    listaSeries[6] = "https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/70975a1227b453c1caf24fa05a93617d.jpe";
+    listaSeries[7] = "https://m.media-amazon.com/images/M/MV5BODRjZGY3ZDktZGIxOC00MWUzLTg1N2EtNDM1NTQ2ZTkxYjc1XkEyXkFqcGdeQXVyMDA4NzMyOA@@._V1_.jpg";
+    listaSeries[8] = "https://sm.ign.com/ign_br/screenshot/default/d-zsr-rvuay-cgh_qnq8.jpg";
+    listaSeries[9] = "https://noticiasdatv.uol.com.br/media/_versions/recomenda/desencanto-netflix-poster_fixed_recomenda_big.png";
+    
+    for(var i=0;i<listaSeries.length;i++){
+        document.write("<img src=" + listaSeries[i] + ">");
+    }
